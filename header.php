1089f0781802798d6079e5fbc87f8bad6e7f488d
gilles.reist <gilles.reist@le-campus-numerique.fr>
2023-06-21 13:07:16 +0200

    lien récupérés avec get

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
         <?=$metaTitle?>>
     </title>
     <meta name="description" content="<?=$metaDescription?>">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="../style.css">
     <script src="https://kit.fontawesome.com/78dd61dcf5.js" crossorigin="anonymous"></script>
 </head>
 <body>
@@ -15,10 +15,10 @@
             <h1>Gilles Reist</h1>
         </div>
         <div id="navbarre">
-            <a href="index.php">C.V.</a><a href="contact.php">Contact</a><a href="hobby.php">Hobby</a>
+            <a href="?page=cv">C.V.</a><a href="?page=contact">Contact</a><a href="?page=hobby">Hobby</a>
         </div>
     </div>
     <div id="photodiv">
-        <img id="photo" src="photo.jpg" alt="Portrait">
+        <img id="photo" src="../photo.jpg" alt="Portrait">
     </div>
 </div>
